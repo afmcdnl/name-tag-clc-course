953d75705b8fad418ee20aaef5dc11a18a634ced
Andrew <afmcdnl@gmail.com>
2022-10-14 08:41:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+// My pronouns
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("He/Him")
+})
+// My Name
+basic.forever(function () {
+    basic.showString("Andrew")
+})
